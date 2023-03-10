f7bffa6069467a508841808b7f656c504fe218e7
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2023-03-10 05:50:36 +0000

    Add files via upload

--- Neuron.h
+++ Neuron.h
@@ -4,6 +4,11 @@
 #include <variant>
 #include <string>
 #include <sstream>
+#include <map>
+#include <vector>
+#include <map>
+#include <variant>
+#include <string>
 
 class Neuron {
 public:
@@ -37,4 +42,40 @@ private:
     double activationFunction(double weightedSum) const {
         return std::max(0.0, weightedSum);
     }
+};
+class NeuronBranch {
+public:
+    std::vector<std::variant<double, std::string>> inputs;
+    std::unique_ptr<Neuron> nextNeuron;
+    std::vector<std::unique_ptr<NeuronBranch>> branches;
+
+    NeuronBranch(const std::vector<std::variant<double, std::string>>& inputs) {
+        this->inputs = inputs;
+        nextNeuron = std::make_unique<Neuron>();
+        nextNeuron->processInputs(inputs);
+    }
+    void addBranch(const std::vector<std::variant<double, std::string>>& inputs) {
+        branches.emplace_back(std::make_unique<NeuronBranch>(inputs));
+    }
+};
+class NeuronTree {
+public:
+    class DataRelationClassification {
+    public:
+        std::map<std::string, int> class_labels;
+
+        void addClassLabel(const std::string& label) {
+            class_labels[label] = class_labels.size() + 1;
+        }
+
+        int getClassLabel(const std::string& label) {
+            return class_labels[label];
+        }
+
+        void classifyData(Neuron& neuron, const std::vector<std::variant<double, std::string>>& inputs) {
+            neuron.inputs = inputs;
+            auto class_label = getClassLabel(std::get<std::string>(inputs.back()));
+            neuron.output = static_cast<double>(class_label);
+        }
+    };
 };
